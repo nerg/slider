760f33ed3b363c7e7e2a8aaca2d6a9f9c3d80edc
Renevey Gilles ZAS <gilles.renevey@zas.admin.ch>
2016-02-16 18:08:02 +0100

    add many sliders

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,17 @@ import { Slider } from './slider.ts';
 
 @Component({
   selector: 'app',
-  template:  `<slider></slider>`, 
+  template:  `    
+    <slider *ngFor="#ci of ciList, #i=index" [slideValue]="ci" [index]="i"></slider>   
+    <button id="printCi" (click)="onClick()">Afficher</button>`, 
   directives: [ Slider ]
 })
 
-export class App {};
+export class App {
+  
+  ciList = [ 123,125,155,123,125,155,123,125,155,123,125,155,123,125,155,123,125,155,235,234,123,125,155,123,125,155,123,125,155,123,125,155,123,125,155,123,125,155,235,234,125,155,235,234 ];   
+  
+  onClick(){
+    alert(this.ciList);
+  }
+};
--- src/slider.ts
+++ src/slider.ts
@@ -1,40 +1,48 @@
-import {Component, ElementRef, Inject, AfterViewInit} from 'angular2/core';
+import {Component, ElementRef, Inject, AfterViewInit, Input} from 'angular2/core';
 
 declare var jQuery:any;
 
 @Component({
     selector: 'slider',
-    template: 
-    `
-    <input [(ngModel)]="slideValue" (keyup)="updSlider()">
-    <div id="slider"></div>
-    <h2>slideValue = {{slideValue}}</h2>
-    `
+    template: `
+      <span class="sliderElement">
+        <div class="sliderInput">
+          <input [(ngModel)]="slideValue" (keyup)="updSlider()" size="1" class="inputSlider" [ngStyle]="{'visibility': visibility}">
+        </div>
+        <div id="slider" class="slider"></div>
+        <h4>{{index}}</h4>
+      <span>`
 })
 
 export class Slider implements AfterViewInit {
-    
+        
+    @Input() slideValue: number;    
     elementRef: ElementRef;
-    slideValue: number;
-
+    visibility = 'hidden';
+    @Input() index: number;
+    
     constructor(@Inject(ElementRef) elementRef: ElementRef) {
       this.elementRef = elementRef;
     }
 
     updSlider() {
       jQuery(this.elementRef.nativeElement).find("#slider")
-      .slider("option", "value", this.slideValue);
+      .slider("option", "value", this.slideValue);      
     }
 
     ngAfterViewInit() { 
       jQuery(this.elementRef.nativeElement).find("#slider").slider({
-        range: false,
+        range: "min",
         orientation: "vertical",
         min: 0,
-        max: 100,
+        max: 300,
+        animate: true,
+        step: 1,
         value: this.slideValue,
         slide: ( event, ui ) => {
           this.slideValue = ui.value;
+          this.visibility = "visible";
+          jQuery(this.elementRef.nativeElement).find(".ui-slider-range").css({"background": "#FFFFcc"});
         }
       });
     }
